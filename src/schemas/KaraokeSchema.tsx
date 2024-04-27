278368d112f6875f20f6e12ef1f30661f7cd8f76
andres941 <andres941cs@gmail.com>
2024-04-27 17:54:49 +0200

    ¨KaraokeSchema¨

--- src/schemas/KaraokeSchema.tsx
+++ src/schemas/KaraokeSchema.tsx
@@ -3,7 +3,10 @@ import { z } from "zod"
 export const KaraokeSchema = z.object({
   id: z.number(),
   settings: z.string(),
+  publication_date: z.string(),
+  isPublished: z.number(),
   id_lyric: z.number(),
+  id_user: z.number(),
 })
 
 export type Karaoke = z.infer<typeof KaraokeSchema>
